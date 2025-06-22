0c24de39ca17e53c314870782ea0c3a5b5126c91
czHappy <41245518+czHappy@users.noreply.github.com>
2025-06-22 11:07:32 +0800

    Create blank.yml
    coverage action

--- build/CMakeFiles/math.dir/src/math.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/math.dir/src/math.cpp.o: \
- /home/plusai/workspace/cpptest/gtest_coverage_cmake/src/math.cpp \
- /usr/include/stdc-predef.h \
- /home/plusai/workspace/cpptest/gtest_coverage_cmake/include/math.h
